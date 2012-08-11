55410b683d719b75a7413fd9de63d45daa9b8c87
ysterio007 <ysterio007@gmail.com>
2012-08-11 17:17:53 +0300

    Update init.php

--- init.php
+++ init.php
@@ -2,7 +2,7 @@
 if(!isset($dbcon)){
 $DB_HOST='instance23872.db.xeround.com:15588';
 $DB_USER='ahmadhammad';
-$DB_PASS='';
+$DB_PASS='ahmad2010';
 $DB_NAME='ahmadhammad1081443';
 
 $dbcon=mysql_connect($DB_HOST,$DB_USER,$DB_PASS);
